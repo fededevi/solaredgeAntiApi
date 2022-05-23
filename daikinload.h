b340790ef47910bbf211e583be1386c1ceb9d09d
Federico Devigili <f.devigili@alpitronic.it>
2022-05-23 14:47:57 +0200

    set lower temp

--- daikinload.h
+++ daikinload.h
@@ -11,7 +11,7 @@ public:
 
     void enable() {
         std::cout << "Turning on Daikin - " << daikinController.host << std::endl;
-        daikinController.setParams(1, ControlMode::AUTO, 25, FanMode::LEVEL_5, WingMode::VERTICAL, 40);
+        daikinController.setParams(1, ControlMode::AUTO, 22, FanMode::LEVEL_5, WingMode::VERTICAL, 40);
     };
 
     void disable() {
